23b3ac38993e44b63bae3b655f94da27ef600d2f
David Shuckerow <djshuckerow@hotmail.com>
2015-03-22 12:58:29 -0500

    OMG IT WERX

--- Translator.fs
+++ Translator.fs
@@ -22,10 +22,10 @@ module Translator =
             let a1, a2 = xlateAexp a1, xlateAexp a2
             Arr.Seq(
                 Arr.Assign("tempbool", Arr.Int(0), Arr.Int(0)),
-                Arr.For("tempbool", Arr.Int(1), a1, a2, 
+                Arr.For("tempbool", Arr.Int(2), a1, a2, 
                     Arr.Seq(
                         Arr.Assign("tempbool", Arr.Int(0), Arr.Int(1)),
-                        Arr.Assign("tempbool", Arr.Int(1), a2)
+                        Arr.Assign("tempbool", Arr.Int(2), a2)
                     )
                 )
             )
@@ -52,7 +52,10 @@ module Translator =
                         )
                     )
                 ),
-                Arr.Assign("tempbool", Arr.Int(0), Arr.Mul(Arr.Arr("tempbool", Arr.Int(0)), Arr.Arr("tempbool", Arr.Int(1))))
+                Arr.Seq(
+                    Arr.Assign("tempbool", Arr.Int(0), Arr.Mul(Arr.Arr("tempbool", Arr.Int(0)), Arr.Arr("tempbool", Arr.Int(1)))),
+                    Arr.Assign("tempbool", Arr.Int(1), Arr.Int(0))
+                )
             )
         | While.Not(b1) -> 
             Arr.Seq(
@@ -85,39 +88,40 @@ module Translator =
         | While.Skip -> Arr.Assign("tempskip", Arr.Int(0), Arr.Int(0))
         | While.Seq(s1, s2) -> Arr.Seq(While2Arr s1, While2Arr s2)
         | While.IfElse(bexp, s1, s2) ->
-            match bexp with
-            | While.True -> (While2Arr (While.IfElse(While.Lte(While.Int(0), While.Int(0)), s1, s2)))
-            | While.False -> While2Arr (While.IfElse(While.Lte(While.Int(1), While.Int(0)), s1, s2))
-            | While.And(bexp1, bexp2) ->
-                While2Arr (While.IfElse(bexp1, While.IfElse(bexp2, s1, s2), s2))
-            | While.Lte(a1, a2) ->
-                Arr.Stm.Seq(
-                    Arr.Stm.For("tempbool", Arr.Int(0), xlateAexp a1, xlateAexp a2, 
-                        Arr.Seq(While2Arr s1, Arr.Assign("tempbool", Arr.Int(0), xlateAexp a2))),
-                    Arr.Stm.For("tempbool", Arr.Int(0), xlateAexp (While.Add(a2, While.Int(1))), xlateAexp a1,
-                        Arr.Seq(While2Arr s2, Arr.Assign("tempbool", Arr.Int(0), xlateAexp a1)))
+            Arr.Seq(
+                xlateBexp bexp,
+                Arr.Seq(
+                    Arr.For("tempif", Arr.Int(0), Arr.Int(1), Arr.Arr("tempbool", Arr.Int(0)),
+                        While2Arr s1
+                    ),
+                    Arr.For("tempif", Arr.Int(0), Arr.Int(1), Arr.Sub(Arr.Int(1), Arr.Arr("tempbool", Arr.Int(0))),
+                        While2Arr s2
+                    )
                 )
-            | While.Eq(a1, a2) -> While2Arr (While.IfElse(While.And(While.Lte(a1, a2), While.Lte(a2, a1)), s1, s2))
-            | While.Not(bexp2) ->
-                match bexp2 with
-                | While.True -> While2Arr (While.IfElse(While.False, s1, s2))
-                | While.False -> While2Arr (While.IfElse(While.True, s1, s2))
-                | _ -> failwith "unhandled"
+            )
         | While.While(bexp, body) ->
-            match bexp with
-            | While.True -> While2Arr (While.While(While.Lte(While.Int(0), While.Int(1)), While.Seq(body, While.Assign("tempbool", While.Int(0))))) /// oh my god, it's so ugly
-            | While.False -> While2Arr (While.While(While.Lte(While.Int(1), While.Int(0)), body))
-            | While.And(bexp1, bexp2) ->
-                let negateBexp (bexp3 : While.Bexp) : While.Aexp =
-                    match bexp3 with
-                    | While.True -> While.Int(1) // change the infinite construct to be 1
-                    | While.Eq(a1, a2) -> While.Add(a1, While.Int(1)) // add 1 to the Aexp to make them no longer equal
-                    | While.Lte(a1, a2) -> While.Add(a2, While.Int(1)) // add 1 here to prevent a weird case from happening
-                    | While.And(a1, a2) -> failwith "What the hell happened here?" // can't have an and as the root of a For
-                    | While.False -> While.Int(1) // This should not affect anything, as the only case it handles is False. 
-                While2Arr (While.While(bexp1, While.IfElse(bexp2, body, While.Assign("tempbool", (negateBexp bexp1)))))
-            | While.Lte(a1, a2) ->
-                Arr.Stm.For("tempbool", Arr.Int(0), xlateAexp a1, xlateAexp a2, While2Arr body)
-            | While.Eq(a1, a2) ->
-                While2Arr (While.While(While.And(While.Lte(a1, a2), While.Lte(a2, a1)), body))
-            | While.Not(bexp1) -> failwith "unhandled"
+            Arr.Seq(
+                Arr.Seq(
+                    Arr.Seq(
+                        xlateBexp bexp,
+                        Arr.Seq(
+                            Arr.Assign("tempcounter", Arr.Int(-1), Arr.Add(Arr.Arr("tempcounter", Arr.Int(-1)), Arr.Int(1))),
+                            Arr.Assign("tempcounter", Arr.Arr("tempcounter", Arr.Int(-1)), Arr.Int(0))
+                        )
+                    ),
+                    (*Do the loop*)
+                    Arr.For("tempcounter", Arr.Int(0), Arr.Arr("tempcounter", Arr.Arr("tempcounter", Arr.Int(-1))), Arr.Sub(Arr.Arr("tempbool", Arr.Int(0)), Arr.Int(1)),
+                        Arr.Seq(
+                            While2Arr body,
+                            Arr.Seq(
+                                xlateBexp bexp,
+                                Arr.Assign("tempcounter", Arr.Arr("tempcounter", Arr.Int(-1)), Arr.Sub(Arr.Arr("tempcounter", Arr.Arr("tempcounter", Arr.Int(-1))), Arr.Int(1)))
+                            )
+                        )
+                    )
+                ),
+                Arr.Seq(
+                    Arr.Assign("tempcounter", Arr.Arr("tempcounter", Arr.Int(-1)), Arr.Int(0)),
+                    Arr.Assign("tempcounter", Arr.Int(-1), Arr.Add(Arr.Arr("tempcounter", Arr.Int(-1)), Arr.Int(-1)))
+                )
+            )
